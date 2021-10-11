7d76158f80c9f1be6ba578ea0b7dc60cf079c6d0
CamiloFr <31927319+CamiloFr@users.noreply.github.com>
2021-10-11 13:23:28 -0500

    update productiron

--- src/config/utils/constanst.js
+++ src/config/utils/constanst.js
@@ -3,5 +3,5 @@ export const ObjConstanst = {
         URL: '../../assets/imagen-no-disponible.png'
     },
     // IP_CULTURE: 'http://127.0.0.1:4000/api/',
-    IP_CULTURE: 'http://20.197.238.29:3000/api/'
+    IP_CULTURE: 'http://20.197.238.29:4000/api/'
 }
