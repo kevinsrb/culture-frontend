9caf2e5371587557ba8d22e0329c46ac97e75864
CamiloFr <31927319+CamiloFr@users.noreply.github.com>
2021-11-05 08:34:29 -0500

    update front end

--- src/config/utils/constanst.js
+++ src/config/utils/constanst.js
@@ -2,7 +2,7 @@ export const ObjConstanst = {
     URL_IMAGENPORDEFECTO:{
         URL: '../../assets/imagen-no-disponible.png'
     },
-    IP_USUARIOS: 'http://127.0.0.1:4000/api/',
-    IP_CULTURE: 'http://127.0.0.1:4001/api/',
-    IP_PARTICIPANTES: 'http://127.0.0.1:4002/api/',
+    IP_USUARIOS: 'http://20.197.238.29:4000/api/',
+    IP_CULTURE: 'http://20.197.238.29:4001/api/',
+    IP_PARTICIPANTES: 'http://20.197.238.29:4002/api/',
 }
